d6ee47e2e06a4b2bf753414ed827f3e7c2f6e666
hanmant <hanmantguddapure@gmail.com>
2020-05-20 10:21:08 +0530

    test

---
+++ src/main/java/com/test/transerve/TranserveApplication.java
@@ -0,0 +1,13 @@
+package com.test.transerve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/transerve/TranserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.transerve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
